c55eec9f00d0d61e2246105e6ce3349b44cd981f
Ronak Shah <ronakshah41@users.noreply.github.com>
2017-06-01 15:14:27 -0500

    changes for homework

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+validates :user_id, :presence => true
+validates :photo_id, :presence => true
+validates :body, :presence => true
+
+belongs_to :photo
+belongs_to :user
+end
